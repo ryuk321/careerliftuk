7bf46906224d9cc4ea2dfd5b1fd5c8f71c142f3c
ryuk321 <Princeshrestha596@gmail.com>
2025-07-30 21:34:46 +0100

    deleted the career page

--- pages/careers.tsx
+++
@@ -1,7 +0,0 @@
-export default function Careers() {
-  return (
-    <main className="min-h-screen bg-gray-50 p-8 flex flex-col items-center justify-center">
-      <div>Careers</div>
-    </main>
-  );
-}
