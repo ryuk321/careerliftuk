321f84c6b6a8cff434b9d70fb02fe08ea7910603
ryuk321 <Princeshrestha596@gmail.com>
2025-07-30 21:25:29 +0100

    Revised Careers.tsx

---
+++ pages/careers.tsx
@@ -0,0 +1,7 @@
+export default function Casreers() {
+  return (
+    <main className="min-h-screen bg-gray-50 p-8 flex flex-col items-center justify-center">
+     <div>Careers</div>
+    </main>
+  );
+}   
