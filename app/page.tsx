5b8224d398b349f315382c454fe15ba368bd5191
ryuk321 <Princeshrestha596@gmail.com>
2025-07-30 21:48:20 +0100

    fixed final

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 
+import Test from "../pages/test"
 import Status from "../componenets/Status"
+
 export default function Home() {
   return (
-    <main className = "p-6 bg-gray-100 min-h-screen">
+    <section className = "p-6 bg-gray-100 min-h-screen">
       <Status />
+      <Test/>
 
-      </main>
+      </section>
   )
 }
